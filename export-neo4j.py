a206f4c9fcd629386a58ea55ffbebbcd2821d9a5
Magnoir <44030687+Magnoir@users.noreply.github.com>
2025-01-14 15:58:27 +0000

    Mes Changements

--- export-neo4j.py
+++ export-neo4j.py
@@ -26,6 +26,7 @@ print("Deleting existing nodes and relationships...")
 graph.run("MATCH ()-[r]->() DELETE r")
 graph.run("MATCH (n:Artist) DETACH DELETE n")
 graph.run("MATCH (n:Film) DETACH DELETE n")
+graph.run("CALL apoc.schema.assert({},{},true) YIELD label, key RETURN *")
 
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     cursor = conn.cursor()
@@ -45,7 +46,10 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
         for row in rows:
             # Créer un objet Node avec comme label Film et les propriétés adéquates
             # A COMPLETER
-            importData.append(n)
+            importData.append({
+                "idFilm" : row[0],
+                "primaryTitle": row[1],
+                "startYear":  row[2]})
             i += 1
 
         try:
@@ -56,17 +60,42 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
             print(error)
 
     # Names
-    # En vous basant sur ce qui a été fait dans la section précédente, exportez les données de la table tNames
+    # En vous basant sur ce qui a été fait dans la section précédente, exportez les données de la table tArtist
     # A COMPLETER
+    exportedCount = 0
+    cursor.execute("SELECT COUNT(1) FROM tArtist")
+    totalCount = cursor.fetchval()
+    cursor.execute("SELECT idArtist, primaryName, birthYear FROM tArtist")
+    while True:
+        importData = []
+        rows = cursor.fetchmany(BATCH_SIZE)
+        if not rows:
+            break
+
+        i = 0
+        for row in rows:
+            # Créer un objet Node avec comme label Artist et les propriétés adéquates
+            # A COMPLETER
+            importData.append({
+                "idArtist" : row[0],
+                "primaryName": row[1],
+                "birthYear":  row[2]})
+            i += 1
+
+        try:
+            create_nodes(graph.auto(), importData, labels={"Artist"})
+            exportedCount += len(rows)
+            print(f"{exportedCount}/{totalCount} title records exported to Neo4j")
+        except Exception as error:
+            print(error)
 
     try:
         print("Indexing Film nodes...")
-        graph.run("CREATE INDEX ON :Film(idFilm)")
+        graph.run("CREATE INDEX FOR (f:Film) ON (f.idFilm)")
         print("Indexing Name (Artist) nodes...")
-        graph.run("CREATE INDEX ON :Artist(idArtist)")
+        graph.run("CREATE INDEX FOR (a:Artist) ON (a.idArtist)")
     except Exception as error:
         print(error)
-
 
     # Relationships
     exportedCount = 0
@@ -82,7 +111,8 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
         for row in rows:
             relTuple=(row[0], {}, row[2])
             importData[row[1]].append(relTuple)
-
+        
+        print(importData)
         try:
             for cat in importData:
                 # Utilisez la fonction create_relationships de py2neo pour créer les relations entre les noeuds Film et Name
@@ -90,7 +120,9 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
                 # https://py2neo.org/2021.1/bulk/index.html
                 # ATTENTION: remplacez les espaces par des _ pour nommer les types de relation
                 # A COMPLETER
-                None # Remplacez None par votre code
+                create_relationships(graph.auto(), importData, "JOUE_DANS", 
+                    start_node_key=("Artist", "idArtist"), 
+                    end_node_key=("Film", "idFilm"))
             exportedCount += len(rows)
             print(f"{exportedCount}/{totalCount} relationships exported to Neo4j")
         except Exception as error:
---
+++ requete-sql.md
@@ -0,0 +1,12 @@
+0)
+SELECT * 
+La table tArtist contient l'ensemble des arti
+
+1)
+2)
+3)
+4)
+5)
+6)
+7)
+8)
