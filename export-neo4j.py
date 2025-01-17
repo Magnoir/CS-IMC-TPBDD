293a85b82f2cee5b7cc15b0f3c0aceaacdf87042
Magnoir <44030687+Magnoir@users.noreply.github.com>
2025-01-17 17:35:26 +0000

    Script d'importation BDD NEO4j

--- export-neo4j.py
+++ export-neo4j.py
@@ -26,7 +26,6 @@ print("Deleting existing nodes and relationships...")
 graph.run("MATCH ()-[r]->() DELETE r")
 graph.run("MATCH (n:Artist) DETACH DELETE n")
 graph.run("MATCH (n:Film) DETACH DELETE n")
-graph.run("CALL apoc.schema.assert({},{},true) YIELD label, key RETURN *")
 
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     cursor = conn.cursor()
@@ -45,11 +44,8 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
         i = 0
         for row in rows:
             # Créer un objet Node avec comme label Film et les propriétés adéquates
-            # A COMPLETER
-            importData.append({
-                "idFilm" : row[0],
-                "primaryTitle": row[1],
-                "startYear":  row[2]})
+            n = Node("Film", idFilm=row[0], primaryTitle=row[1], startYear=row[2])
+            importData.append(n)
             i += 1
 
         try:
@@ -61,7 +57,6 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
 
     # Names
     # En vous basant sur ce qui a été fait dans la section précédente, exportez les données de la table tArtist
-    # A COMPLETER
     exportedCount = 0
     cursor.execute("SELECT COUNT(1) FROM tArtist")
     totalCount = cursor.fetchval()
@@ -75,25 +70,22 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
         i = 0
         for row in rows:
             # Créer un objet Node avec comme label Artist et les propriétés adéquates
-            # A COMPLETER
-            importData.append({
-                "idArtist" : row[0],
-                "primaryName": row[1],
-                "birthYear":  row[2]})
+            n = Node("Artist", idArtist=row[0], primaryName=row[1], birthYear=row[2])
+            importData.append(n)
             i += 1
 
         try:
             create_nodes(graph.auto(), importData, labels={"Artist"})
             exportedCount += len(rows)
-            print(f"{exportedCount}/{totalCount} title records exported to Neo4j")
+            print(f"{exportedCount}/{totalCount} artists exported to Neo4j")
         except Exception as error:
             print(error)
 
     try:
         print("Indexing Film nodes...")
-        graph.run("CREATE INDEX FOR (f:Film) ON (f.idFilm)")
+        graph.run("CREATE INDEX FOR (n:Film) ON (n.idFilm)")
         print("Indexing Name (Artist) nodes...")
-        graph.run("CREATE INDEX FOR (a:Artist) ON (a.idArtist)")
+        graph.run("CREATE INDEX FOR (n:Artist) ON (n.idArtist)")
     except Exception as error:
         print(error)
 
@@ -111,18 +103,14 @@ with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE=
         for row in rows:
             relTuple=(row[0], {}, row[2])
             importData[row[1]].append(relTuple)
-        
-        print(importData)
+
         try:
             for cat in importData:
                 # Utilisez la fonction create_relationships de py2neo pour créer les relations entre les noeuds Film et Name
                 # (les tuples nécessaires ont déjà été créés ci-dessus dans la boucle for précédente)
                 # https://py2neo.org/2021.1/bulk/index.html
                 # ATTENTION: remplacez les espaces par des _ pour nommer les types de relation
-                # A COMPLETER
-                create_relationships(graph.auto(), importData, "JOUE_DANS", 
-                    start_node_key=("Artist", "idArtist"), 
-                    end_node_key=("Film", "idFilm"))
+                create_relationships(graph.auto(), importData[cat], cat.replace(" ", "_").upper(), start_node_key=("Artist", "idArtist"), end_node_key=("Film", "idFilm"))
             exportedCount += len(rows)
             print(f"{exportedCount}/{totalCount} relationships exported to Neo4j")
         except Exception as error:
